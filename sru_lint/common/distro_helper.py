0be336944201a90ad08a6ee14b6452929e83d6d2
Dariusz Gadomski <dgadomski@ubuntu.com>
2026-05-13 12:38:54 +0200

    distro-helper: added helper methods.

--- sru_lint/common/distro_helper.py
+++ sru_lint/common/distro_helper.py
@@ -1,4 +1,8 @@
+import re
+
 from distro_info import UbuntuDistroInfo
+
+_ESM_SUFFIX_RE = re.compile(r"~esm\d+")
 
 
 def get_esm_only_releases() -> list[str]:
@@ -7,3 +11,13 @@ def get_esm_only_releases() -> list[str]:
     esm = set(info.supported_esm())
     supported = set(info.supported())
     return sorted(esm - supported)
+
+
+def is_esm_only_release(release: str) -> bool:
+    """Return True if the given release is supported under ESM but no longer in standard support."""
+    return release in get_esm_only_releases()
+
+
+def has_esm_suffix(version: str) -> bool:
+    """Check if the version string contains an ``~esm<number>`` suffix."""
+    return _ESM_SUFFIX_RE.search(version) is not None
